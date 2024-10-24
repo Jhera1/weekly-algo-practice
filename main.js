e8910305aad04e9d81f2d144abc08715204002c1
Justin Armstrong <armstrongjustin034@gmail.com>
2024-10-24 11:36:39 -0700

    updated #12

--- main.js
+++ main.js
@@ -83,10 +83,10 @@ const findLongestWord = (str) => {
     let longestWord = '';  
 
     // Loop through the array of words
-    for (let word of words) {
+    for (const word of words) {
         // If the current word's length is greater than the longest word's length, update longestWord
         if (word.length > longestWord.length) {
-            longestWord = word;
+            longestWord = word; 
         }
     }
 

d90c35ade77f0ff692323e908ecdc33e18cabf98
Justin Armstrong <armstrongjustin034@gmail.com>
2025-07-04 12:18:26 -0700

    completed exrention

---
+++ moveZerosToEnd.js
@@ -0,0 +1,36 @@
+// # 29 Move Zereo's to the End
+// Eric.Devslopes
+// OP
+//  — 06/09/2025 9:27 AM
+// Write a function that takes an array of 
+// numbers and moves all zeroes to the end while maintaining 
+// the relative order of non-zero elements 
+
+// Extension: Modify the function to minimize the total 
+// number of operations.
+
+
+// const moveZereosToEnd = arr => {
+//   let nonZeros = arr.filter(a => a !== 0)
+//   let zeros = arr.filter(a => a === 0)
+//   return nonZeros.concat(zeros)
+// }
+
+// Extention
+
+function moveZereosToEnd(arr) {
+  let placePositiveAtIndex = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[placePositiveAtIndex++] = arr[i];
+    }
+  }
+  while (placePositiveAtIndex < arr.length) {
+    arr[placePositiveAtIndex++] = 0;
+  }
+  return arr;
+}
+
+
+console.log(moveZereosToEnd([0, 1, 0, 3])) // => [1, 3, 0, 0]

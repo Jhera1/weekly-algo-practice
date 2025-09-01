0e0a127145bfce300d1939cf69ccfc5e30d2f9a6
Justin Armstrong <armstrongjustin034@gmail.com>
2025-09-01 16:30:25 -0700

    added isPrime.js

---
+++ isPrime.js
@@ -0,0 +1,9 @@
+// "Prompt: Write a function that determines if a number
+//  is prime. 
+
+const isPrime = n => {
+    
+}
+
+// Extension: Modify the function to return an array 
+// of all prime numbers less than the given number."

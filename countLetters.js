058f96217aa28ce637d1655a145bf164a6d846e8
Justin Armstrong <armstrongjustin034@gmail.com>
2025-09-01 18:59:32 -0700

    completed isPrime.js & added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,12 @@
+// Prompt: Write a function that takes a string and 
+// returns a dictionary with each character as the key 
+// and the number of times it appears in the string as 
+// the value.
+
+// Extension: Modify the function to ignore spaces and 
+// punctuation, and count letters case-insensitively.
+
+// Extension: Modify the function to return the kth most 
+// commonly occurring letter. 
+// Example: ("Hello, World!", 2) would return the 
+// 2nd most common letter in the string. In this case,"o"!
--- isPrime.js
+++ isPrime.js
@@ -1,9 +1,40 @@
 // "Prompt: Write a function that determines if a number
 //  is prime. 
 
-const isPrime = n => {
-    
-}
+// const isPrime = n => {
+//     if (n < 2) return false
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//         if (n % i === 0) return false
+//     }
+//     return true
+//   }
+  
+//   console.log(isPrime(7))  // true
+//   console.log(isPrime(6))  // false
+//   console.log(isPrime(9))  // false
+//   console.log(isPrime(13)) // true
+  
 
 // Extension: Modify the function to return an array 
 // of all prime numbers less than the given number."
+
+const arrOfPrimes = n => {
+    let result = []
+    for (let i = 2; i <= n; i++) {
+        let prime = true
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                prime = false
+                break
+            }
+        }
+        if (prime) {
+            result.push(i)
+        }
+    }
+    return result
+}
+  console.log(arrOfPrimes(7)) // => [2, 3, 5, 7]
+
+
+      

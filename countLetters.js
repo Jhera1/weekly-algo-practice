659af4412886bd59211d1ae8be9546a470286527
Justin Armstrong <armstrongjustin034@gmail.com>
2025-09-02 18:16:38 -0700

    completed count letters & longestWord

--- countLetters.js
+++ countLetters.js
@@ -6,7 +6,29 @@
 // Extension: Modify the function to ignore spaces and 
 // punctuation, and count letters case-insensitively.
 
+// const strToMap = str => {
+//     let letterMap = {}
+//     let lowerStr = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+//     for (let letter of lowerStr) {
+//         letterMap[letter] = (letterMap[letter] || 0) + 1
+//     }
+//     return letterMap
+// }
+// console.table(strToMap('Somewhere in Time?'))
+
+
+
 // Extension: Modify the function to return the kth most 
 // commonly occurring letter. 
 // Example: ("Hello, World!", 2) would return the 
 // 2nd most common letter in the string. In this case,"o"!
+
+const strToMap = (str, k) => {
+    let letterMap = {}
+    let lowerStr = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+    for (let letter of lowerStr) {
+        letterMap[letter] = (letterMap[letter] || 0) + 1
+    }
+    return Object.entries(letterMap).sort((a, b) => b[1] - a[1], 0)[k - 1][0]
+}
+console.table(strToMap('Hello, World', 2))
---
+++ longestWord.js
@@ -0,0 +1,36 @@
+// Prompt: Write a function that takes a string 
+// as input and returns the longest word in the string.
+
+// const longestWord = str => {
+//     let splitString = str.split(' ')
+//     let longestWord = null
+//     let count = -Infinity
+//     for (let word of splitString) {
+//       if (word.length > count){
+//         longestWord = word
+//         count = word.length
+//       }
+//     }
+//     return longestWord
+// }
+// console.log(longestWord('These frogs leap over the couch'))
+
+// Extension: Modify the function to return an array 
+// of all longest words (in case there are ties).
+
+const longestWords = str => {
+    let splitString = str.split(' ')
+    let longestWords = []
+    let count = -Infinity
+    for (let word of splitString) {
+      if (word.length > count) {
+        longestWords = [word]
+        count = word.length
+      }  else if (word.length === count) {
+        longestWords.push(word)
+      }
+      
+    }
+    return longestWords
+}
+console.log(longestWords('These frogs leap over the couch'))

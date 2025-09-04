af9d5981c1c5cf05d48d00d9d851f12ecb509d53
Justin Armstrong <armstrongjustin034@gmail.com>
2025-09-04 13:12:56 -0700

    completedrotateArr.js & added genFib.js

--- rotateArr.js
+++ rotateArr.js
@@ -2,7 +2,16 @@
 // number k as input and rotates the array to the right 
 // by k steps.
 
-
+// const rotateArr = (arr, k) => arr.slice(-k).concat(arr.slice(0, -k))
 
 // Extension: Modify the function to handle negative 
 // values of k to rotate the array to the left.
+
+const rotateArr= (arr, k) => {
+   const n = arr.length
+   k = ((k % n) + n) % n
+   
+   return    arr.slice(-k).concat(arr.slice(0, -k))
+
+}
+console.log(rotateArr([1,2,3,4,5], 7))

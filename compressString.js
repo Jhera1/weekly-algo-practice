ee8816ed4e7378f3f5554efe513c01e709cb2a3d
Justin Armstrong <armstrongjustin034@gmail.com>
2025-09-04 19:11:04 -0700

    completed compressString.js & added validateSoduku.js

--- compressString.js
+++ compressString.js
@@ -3,5 +3,26 @@
 // For example, the string "aabcccccaaa" would become
 //  "a2b1c5a3".
 
+const compressString = str => {
+    if (!str) return ""
+  
+    let result = ""
+    let count = 1
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === str[i + 1]) {
+      count++
+    } else {
+        result += str[i].concat(count)
+        count = 1
+    }
+  } return result
+}
+  
+  console.log(compressString("aabcccccaaa")) // "a2b1c5a3"
+  console.log(compressString("abc"))         // "a1b1c1"
+  console.log(compressString("aaabb"))       // "a3b2"
+  
+
 // Extension : Modify the function to return the original
 // string if the compressed string would not be smaller.
---
+++ validateSoduku.js
@@ -0,0 +1,5 @@
+// Determine if a 9x9 Sudoku board is valid. A valid 
+// Sudoku board (partially filled) must not violate 
+// the rules of Sudoku. The sudoku board input is 
+// represented by a 2D array containing either string 
+// numbers or "." (empty space) as elements. 
